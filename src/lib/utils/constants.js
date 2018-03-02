f08cf5ea8b6e375603dbbbb18de8b2c7324b0554
doubleswirve <saxon.stiller@gmail.com>
2018-03-02 18:52:32 -0500

    port over operators and associated helpers for flow

--- src/lib/utils/constants.js
+++ src/lib/utils/constants.js
@@ -1,4 +1,6 @@
 // @flow
+import type { Operator } from './Operator'
+
 const FIELD_TYPE_STRING = 'string'
 const FIELD_TYPE_INTEGER = 'integer'
 const FIELD_TYPE_DOUBLE = 'double'
@@ -10,6 +12,251 @@ const FIELD_TYPE_BOOLEAN = 'boolean'
 const QB_RULE = 'QBRule'
 const QB_GROUP = 'QBGroup'
 
+const OPERATORS: Array<Operator> = [
+  {
+    id: 'equal',
+    meta: {
+      fieldTypes: [
+        FIELD_TYPE_STRING,
+        FIELD_TYPE_INTEGER,
+        FIELD_TYPE_DOUBLE,
+        FIELD_TYPE_DATE,
+        FIELD_TYPE_TIME,
+        FIELD_TYPE_DATETIME,
+        FIELD_TYPE_BOOLEAN
+      ],
+      multipleValuesAllowed: false,
+      numberOfInputs: 1
+    }
+  },
+  {
+    id: 'not_equal',
+    meta: {
+      fieldTypes: [
+        FIELD_TYPE_STRING,
+        FIELD_TYPE_INTEGER,
+        FIELD_TYPE_DOUBLE,
+        FIELD_TYPE_DATE,
+        FIELD_TYPE_TIME,
+        FIELD_TYPE_DATETIME,
+        FIELD_TYPE_BOOLEAN
+      ],
+      multipleValuesAllowed: false,
+      numberOfInputs: 1
+    }
+  },
+  {
+    id: 'in',
+    meta: {
+      fieldTypes: [
+        FIELD_TYPE_STRING,
+        FIELD_TYPE_INTEGER,
+        FIELD_TYPE_DOUBLE,
+        FIELD_TYPE_DATE,
+        FIELD_TYPE_TIME,
+        FIELD_TYPE_DATETIME
+      ],
+      multipleValuesAllowed: true,
+      numberOfInputs: 1
+    }
+  },
+  {
+    id: 'not_in',
+    meta: {
+      fieldTypes: [
+        FIELD_TYPE_STRING,
+        FIELD_TYPE_INTEGER,
+        FIELD_TYPE_DOUBLE,
+        FIELD_TYPE_DATE,
+        FIELD_TYPE_TIME,
+        FIELD_TYPE_DATETIME
+      ],
+      multipleValuesAllowed: true,
+      numberOfInputs: 1
+    }
+  },
+  {
+    id: 'less',
+    meta: {
+      numberOfInputs: 1,
+      multipleValuesAllowed: false,
+      fieldTypes: [
+        FIELD_TYPE_INTEGER,
+        FIELD_TYPE_DOUBLE,
+        FIELD_TYPE_DATE,
+        FIELD_TYPE_TIME,
+        FIELD_TYPE_DATETIME
+      ],
+    }
+  },
+  {
+    id: 'less_or_equal',
+    meta: {
+      fieldTypes: [
+        FIELD_TYPE_INTEGER,
+        FIELD_TYPE_DOUBLE,
+        FIELD_TYPE_DATE,
+        FIELD_TYPE_TIME,
+        FIELD_TYPE_DATETIME
+      ],
+      multipleValuesAllowed: false,
+      numberOfInputs: 1
+    }
+  },
+  {
+    id: 'greater',
+    meta: {
+      fieldTypes: [
+        FIELD_TYPE_INTEGER,
+        FIELD_TYPE_DOUBLE,
+        FIELD_TYPE_DATE,
+        FIELD_TYPE_TIME,
+        FIELD_TYPE_DATETIME
+      ],
+      multipleValuesAllowed: false,
+      numberOfInputs: 1
+    }
+  },
+  {
+    id: 'greater_or_equal',
+    meta: {
+      fieldTypes: [
+        FIELD_TYPE_INTEGER,
+        FIELD_TYPE_DOUBLE,
+        FIELD_TYPE_DATE,
+        FIELD_TYPE_TIME,
+        FIELD_TYPE_DATETIME
+      ],
+      multipleValuesAllowed: false,
+      numberOfInputs: 1
+    }
+  },
+  {
+    id: 'between',
+    meta: {
+      fieldTypes: [
+        FIELD_TYPE_INTEGER,
+        FIELD_TYPE_DOUBLE,
+        FIELD_TYPE_DATE,
+        FIELD_TYPE_TIME,
+        FIELD_TYPE_DATETIME
+      ],
+      multipleValuesAllowed: false,
+      numberOfInputs: 2
+    }
+  },
+  {
+    id: 'not_between',
+    meta: {
+      fieldTypes: [
+        FIELD_TYPE_INTEGER,
+        FIELD_TYPE_DOUBLE,
+        FIELD_TYPE_DATE,
+        FIELD_TYPE_TIME,
+        FIELD_TYPE_DATETIME
+      ],
+      multipleValuesAllowed: false,
+      numberOfInputs: 2
+    }
+  },
+  {
+    id: 'begins_with',
+    meta: {
+      fieldTypes: [FIELD_TYPE_STRING],
+      multipleValuesAllowed: false,
+      numberOfInputs: 1
+    }
+  },
+  {
+    id: 'not_begins_with',
+    meta: {
+      fieldTypes: [FIELD_TYPE_STRING],
+      multipleValuesAllowed: false,
+      numberOfInputs: 1
+    }
+  },
+  {
+    id: 'contains',
+    meta: {
+      fieldTypes: [FIELD_TYPE_STRING],
+      multipleValuesAllowed: false,
+      numberOfInputs: 1
+    }
+  },
+  {
+    id: 'not_contains',
+    meta: {
+      fieldTypes: [FIELD_TYPE_STRING],
+      multipleValuesAllowed: false,
+      numberOfInputs: 1
+    }
+  },
+  {
+    id: 'ends_with',
+    meta: {
+      fieldTypes: [FIELD_TYPE_STRING],
+      multipleValuesAllowed: false,
+      numberOfInputs: 1
+    }
+  },
+  {
+    id: 'not_ends_with',
+    meta: {
+      fieldTypes: [FIELD_TYPE_STRING],
+      multipleValuesAllowed: false,
+      numberOfInputs: 1
+    }
+  },
+  {
+    id: 'is_empty',
+    meta: {
+      fieldTypes: [FIELD_TYPE_STRING],
+      multipleValuesAllowed: false,
+      numberOfInputs: 0
+    }
+  },
+  {
+    id: 'is_not_empty',
+    meta: {
+      fieldTypes: [FIELD_TYPE_STRING],
+      multipleValuesAllowed: false,
+      numberOfInputs: 0
+    }
+  },
+  {
+    id: 'is_null',
+    meta: {
+      fieldTypes: [
+        FIELD_TYPE_STRING,
+        FIELD_TYPE_INTEGER,
+        FIELD_TYPE_DOUBLE,
+        FIELD_TYPE_DATE,
+        FIELD_TYPE_TIME,
+        FIELD_TYPE_DATETIME,
+        FIELD_TYPE_BOOLEAN
+      ],
+      multipleValuesAllowed: false,
+      numberOfInputs: 0
+    }
+  },
+  {
+    id: 'is_not_null',
+    meta: {
+      fieldTypes: [
+        FIELD_TYPE_STRING,
+        FIELD_TYPE_INTEGER,
+        FIELD_TYPE_DOUBLE,
+        FIELD_TYPE_DATE,
+        FIELD_TYPE_TIME,
+        FIELD_TYPE_DATETIME,
+        FIELD_TYPE_BOOLEAN
+      ],
+      multipleValuesAllowed: false,
+      numberOfInputs: 0
+    }
+  }
+]
+
 export {
   FIELD_TYPE_STRING,
   FIELD_TYPE_INTEGER,
@@ -19,5 +266,6 @@ export {
   FIELD_TYPE_DATETIME,
   FIELD_TYPE_BOOLEAN,
   QB_RULE,
-  QB_GROUP
+  QB_GROUP,
+  OPERATORS
 }
--- src/lib/utils/operators.js
+++ src/lib/utils/operators.js
@@ -1,2 +1,23 @@
 // @flow
-// TODO: ...
+import type { Operator } from './Operator'
+import { OPERATORS } from './constants'
+
+function getAllOperatorsByFieldType (fieldType: string): Array<Operator> {
+  return OPERATORS.filter(operator => {
+    return operator.meta.fieldTypes.includes(fieldType)
+  })
+}
+
+function getDefaultOperatorByFieldType (fieldType: string): ?Operator {
+  return getAllOperatorsByFieldType(fieldType)[0]
+}
+
+function getOperatorById (id: string): ?Operator {
+  return OPERATORS.find(operator => operator.id === id)
+}
+
+export {
+  getAllOperatorsByFieldType,
+  getDefaultOperatorByFieldType,
+  getOperatorById
+}
