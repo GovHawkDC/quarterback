16a92c09c30bf5a45048d0748c0ee583e893e52a
doubleswirve <saxon.stiller@gmail.com>
2018-03-05 15:15:48 -0500

    update prop name to be consistent with group level and field custom component

--- src/lib/utils/Field.js
+++ src/lib/utils/Field.js
@@ -2,7 +2,7 @@
 import * as React from 'react'
 
 export type Field = {
-  component?: React.ComponentType<>, // TODO: Props
+  QBComponent?: React.ComponentType<>, // TODO: Props
   id: string,
   input: string, // TODO: Constrain to set?
   label: string,
