8650aa70ef53de81f9ab5e3e77b111bed6a22e40
doubleswirve <saxon.stiller@gmail.com>
2018-06-01 07:57:47 -0700

    update default action displays for rules and groups

--- src/lib/utils/actions.js
+++ src/lib/utils/actions.js
@@ -23,7 +23,7 @@ function getActionByType (type: Type): Action {
 
 const ruleAction: Action = {
   QB: QB_RULE,
-  display: 'Add Rule',
+  display: 'Rule',
   getDefaultData: function () {
     return {
       QB: this.QB,
@@ -39,7 +39,7 @@ const ruleAction: Action = {
 
 const groupAction: Action = {
   QB: QB_GROUP,
-  display: 'Add Group',
+  display: 'Group',
   getDefaultData: function (fields, defaultCondition) {
     const rules = fields != null && fields.length > 0
       ? [ruleAction.getDefaultData()]
