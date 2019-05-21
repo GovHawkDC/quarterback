62f2fd484387c2c8eec273004d2d62586474e681
doubleswirve <863571+doubleswirve@users.noreply.github.com>
2019-05-21 13:35:57 -0600

    refactor search fields by id helper to be recursive (allowing for nested fields, ie optgroups)

--- src/lib/utils/fields.js
+++ src/lib/utils/fields.js
@@ -3,7 +3,17 @@ import * as React from 'react'
 import type { Field } from './Field'
 
 function getFieldById (fields: Array<Field>, id: string): ?Field {
-  return fields.find(field => field.id === id)
+  for (let f of fields) {
+    if (f.fields) {
+      let match = getFieldById(f.fields, id)
+
+      if (match) {
+        return match
+      }
+    } else if (f.id === id) {
+      return f
+    }
+  }
 }
 
 function getFieldComponent (field: Field, DefaultComponent: React.Component): React.Component {
