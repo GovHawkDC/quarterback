3884ebad9a5feb306b6a373cda4ab8f9d26b7fe5
doubleswirve <863571+doubleswirve@users.noreply.github.com>
2019-07-02 13:28:58 -0600

    filter out fields containing true deleted key so users can't use the field (even though it might display for saved values)

--- src/lib/components/QuarterBackFields.js
+++ src/lib/components/QuarterBackFields.js
@@ -56,7 +56,7 @@ class QuarterBackFields extends React.Component<Props> {
     } = this.props
 
     const defaultValue = { label: selectPlaceholder, id: '' }
-    const values = [ defaultValue, ...fields ]
+    const values = [ defaultValue, ...fields.filter(f => !f.deleted) ]
 
     const addClass = styleClassMap.QuarterBackFields || ''
 
